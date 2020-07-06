199a8b18378ff20549fcd50f0690f94aeb2b866f
Giacomo <giacomo.trovo.1@studenti.unipd.it>
2020-07-06 13:11:28 +0200

    + more non tree images + additionl  metric SSIM to detect whether a selected tree is a tree or not -> experiments to find a threshold + 2 files for the results of the avg SSIM

--- src/main.cpp
+++ src/main.cpp
@@ -53,6 +53,8 @@ vector<treeData> refineWithFeatureMatching(Mat inputImage, string templateFeatur
 double zncc(Mat inputRect, Mat templ);
 double avgZncc(Mat inputRect, string templPath);
 void visualizeResults(vector<treeData> selectTrees, Mat inputImg);
+bool isATree(Mat rectangle, string templateImages);
+Scalar getMSSIM(const Mat& i1, const Mat& i2);
 
 
 //double computeZNCC(Mat img, Mat templImg);
@@ -462,14 +464,32 @@ void visualizeResults(vector<treeData> trees, Mat inputImg) {
 		cout << "NO TREES DETECTED!" << endl;
 		return;
 	}
-	cout << "Final detected tree - method: " << status << endl;
-	printTreeData(selectedTree);
+	//check with SSIM similarity metric whether the selected tree is a tree or not
+	if (isATree(inputImg(selectedTree.rect), "../data/template")) {
+		cout << "Final detected tree - method: " << status << endl;
+		printTreeData(selectedTree);
 
-	rectangle(inputImg, selectedTree.rect, Scalar(125), 2);
-	namedWindow("Final detection", WINDOW_NORMAL);
-	imshow("Final detection", inputImg);
-	waitKey(0);
+		rectangle(inputImg, selectedTree.rect, Scalar(125), 2);
+		namedWindow("Final detection", WINDOW_NORMAL);
+		imshow("Final detection", inputImg);
+		waitKey(0);
+	}
 
+}
+
+bool isATree(Mat rectangle, string templateImages) {
+	vector<String> files;
+	utils::fs::glob(templateImages, "*.*", files);
+	double avgSSIM = 0.0;
+	for (int i = 0; i < files.size(); i++) {
+		Mat templImg = imread(files[i]);
+		resize(templImg, templImg, rectangle.size()); //resize template image to the size of the detected rectangle
+		Scalar res = getMSSIM(templImg, rectangle);
+		avgSSIM += mean(res)[0];
+		//cout << "template file: " << files[i] << " score: " << mean(res)[0] << endl;
+	}
+	avgSSIM /= files.size();
+	cout << " score: " << avgSSIM << endl;
 }
 
 vector<treeData> refineWithFeatureMatching(Mat inputImage, string templateFeaturePath, vector<treeData> selecTrees) {
@@ -536,6 +556,68 @@ void printTreeData(treeData data){
 	cout << "Rect: " << data.rect << " Overlapping rectangle: " << data.numOverlRect << " Score overlapping rect: " << data.scoreOverlRect << " score2: " << (data.scoreOverlRect/ data.numOverlRect) <<  endl;
 }
 
+Scalar getMSSIM(const Mat& i1, const Mat& i2)
+{
+	const double C1 = 6.5025, C2 = 58.5225;
+	/***************************** INITS **********************************/
+	int d = CV_32F;
+
+	Mat I1, I2;
+	i1.convertTo(I1, d);           // cannot calculate on one byte large values
+	i2.convertTo(I2, d);
+
+	Mat I2_2 = I2.mul(I2);        // I2^2
+	Mat I1_2 = I1.mul(I1);        // I1^2
+	Mat I1_I2 = I1.mul(I2);        // I1 * I2
+
+	/*************************** END INITS **********************************/
+
+	Mat mu1, mu2;   // PRELIMINARY COMPUTING
+	GaussianBlur(I1, mu1, Size(11, 11), 1.5);
+	GaussianBlur(I2, mu2, Size(11, 11), 1.5);
+
+	Mat mu1_2 = mu1.mul(mu1);
+	Mat mu2_2 = mu2.mul(mu2);
+	Mat mu1_mu2 = mu1.mul(mu2);
+
+	Mat sigma1_2, sigma2_2, sigma12;
+
+	GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
+	sigma1_2 -= mu1_2;
+
+	GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
+	sigma2_2 -= mu2_2;
+
+	GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
+	sigma12 -= mu1_mu2;
+
+	///////////////////////////////// FORMULA ////////////////////////////////
+	Mat t1, t2, t3;
+
+	t1 = 2 * mu1_mu2 + C1;
+	t2 = 2 * sigma12 + C2;
+	t3 = t1.mul(t2);              // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+
+	t1 = mu1_2 + mu2_2 + C1;
+	t2 = sigma1_2 + sigma2_2 + C2;
+	t1 = t1.mul(t2);               // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+
+	Mat ssim_map;
+	divide(t3, t1, ssim_map);      // ssim_map =  t3./t1;
+
+	Scalar mssim = mean(ssim_map); // mssim = average of ssim map
+	return mssim;
+}
+
+
+
+
+
+
+
+
+
+
 void extractFeatures(Mat img, vector<KeyPoint> &features, Mat &desciptors, int numFeatures) {
 	Ptr<xfeatures2d::SIFT> detector = xfeatures2d::SIFT::create(numFeatures);
 	detector->detectAndCompute(img, Mat(), features, desciptors);
@@ -576,7 +658,6 @@ void computeMatchesFlann(Mat templateDescriptors, Mat imageDescriptors, vector<D
 	cout << "matches size: " << matches.size() << endl;
 
 }
-
 
 int printRectangle(Mat &frame, vector<Point2f> corners, Scalar color) {
 	if (corners.size() != 4) return -1;
@@ -671,7 +752,6 @@ void templateSegmentation(Mat img, string pathTemplate) {
 
 
 }
-
 
 void watershedSegmentation(Mat img, Mat &segmentedImg) {
 	Mat bw, bw1, bw2, dist, imgBlur;
